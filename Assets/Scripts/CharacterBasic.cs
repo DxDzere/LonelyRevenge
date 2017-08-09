517cb0ac23558e68b62eb8a28f70ca978b8fd6f2
IvanSimao <ivanezequielsimao@gmail.com>
2017-08-09 11:38:02 -0300

    base de personajes, npc, y enemigos

---
+++ Assets/Scripts/CharacterBasic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBasic : MonoBehaviour
+{
+    [SerializeField]
+    struct baseStats
+    {
+        int healt;
+        int healtCap;
+        int attack;
+        int defense;
+        int mana;
+        int manaCap;
+        int dexterity;
+    }
+    string name;
+    enum type {Warrior, SpellCaster, Rogue, Priest, Archer}
+    enum state {Idle, Walk, Run, Attack, Dead, Hit, Jump}
+    //anims
+    //weapons
+    string description;
+    Vector3 pos;
+}
---
+++ Assets/Scripts/Player Base.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    [SerializeField]
+    struct modificableStats
+    {
+        int healtCap;
+        int attack;
+        int defense;
+        int manaCap;
+        int dexterity;
+    }
+    //controller
+    int xp, xpCap, lvl, lvlCap;
+    //inventory
+    //stat modification
+}
