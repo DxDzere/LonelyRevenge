0287bd25a09f4fc9501078a8e3b4f7fa4e75c72e
zere-PC\zere <nikorasugema@gmail.com>
2017-11-06 23:01:40 -0300

    nicolas
    Correccuin en godigo de texto

--- Assets/Scripts/StrengthUI.cs
+++ Assets/Scripts/StrengthUI.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StrengthUI : MonoBehaviour {
 
-    public GUIText strengthText;
-    public PlayerBase playerStat;
+    public Text nameText;
+    public Text classhText;
+    public Text levelText;
+    public Text expText;
+    public Text atributsPointsText;
+    public Text strengthText;
+    public Text dexterityText;
+    public Text vitalityText;
+    public Text energyText;
+    public Text goldText;
+    public PlayerBase player;
 
 	// Use this for initialization
 	void Start ()
@@ -15,7 +25,20 @@ public class StrengthUI : MonoBehaviour {
 	
     public void UpdateText()
     {
-        strengthText.text = Mathf.RoundToInt(playerStat.atributes.strength).ToString();
+        nameText.text = player.name;
+        classhText.text = player.kind.ToString();
+        levelText.text = Mathf.RoundToInt(player.lvl).ToString();
+        expText.text = Mathf.RoundToInt(player.xp).ToString();
+        atributsPointsText.text = Mathf.RoundToInt(player.atributesPoint).ToString();
+        strengthText.text = Mathf.RoundToInt(player.atributes.strength).ToString();
+        dexterityText.text = Mathf.RoundToInt(player.atributes.dexterity).ToString();
+        vitalityText.text = Mathf.RoundToInt(player.atributes.vitality).ToString();
+        energyText.text = Mathf.RoundToInt(player.atributes.energy).ToString();
+        goldText.text = Mathf.RoundToInt(player.money).ToString();
     }
-	
+
+    private void Update()
+    {
+        UpdateText();
+    }
 }
